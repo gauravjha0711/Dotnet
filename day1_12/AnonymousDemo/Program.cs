9431163afc31385cac3f2670fd768a1d240b947f
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-12 15:02:35 +0530

    Anonymous

---
+++ day1_12/AnonymousDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousDemo
+{
+    public delegate void Print(int val, string str);
+    public delegate void Print1(int val);
+    internal class Program
+    {
+        public static void PrintHelperMethod(Print1 printDel, int val)
+        {
+            val += 10;
+            printDel(val);
+        }
+        public static void Main(string[] args)
+        {
+            PrintHelperMethod(delegate (int val)
+            {
+                Console.WriteLine($"Anonymous Method called with value: {val}");
+            }, 100);
+
+            Print print = delegate (int val, string str)
+            {
+                Console.WriteLine($"Hello World {val} {str}");
+            };
+            print(100, "Welcome");
+            var ManagerInfo = new
+            {
+                id = 1001,
+            Name = "Gaurav Jha"
+            };
+            
+            Console.WriteLine($"Manager id is {ManagerInfo.id}");
+            Console.WriteLine($"Manager name is {ManagerInfo.Name} ");
+            Console.ReadLine();
+        }
+    }
+}
--- day1_12/AnonymousDemo/Properties/AssemblyInfo.cs
+++ day1_12/AnonymousDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ananonemes")]
+[assembly: AssemblyTitle("AnonymousDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ananonemes")]
+[assembly: AssemblyProduct("AnonymousDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d86f73e3-d668-4523-921c-217fd18e1ddf")]
+[assembly: Guid("4b92b5d1-b21c-4025-b3ee-97e935134211")]
 
 // Version information for an assembly consists of the following four values:
 //
