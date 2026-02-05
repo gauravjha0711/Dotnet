d96f91bd5b32926a22bfdb471fea197f1455dfd7
gauravjha0711 <gauravjha0711@gmail.com>
2026-02-05 10:38:57 +0530

    Read from database

---
+++ ADO2/ConnectedEnv.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO2
+{
+    internal class ConnectedEnv
+    {
+        string ConString = @"Data Source=DESKTOP-84AGQQF\SQLEXPRESS2025;Initial Catalog = SchoolDB; Integrated Security = True;";
+        SqlDataReader dataReader;
+        public void GetAllStudent()
+        {
+            string sqlCmd = "SELECT * FROM students";        
+            SqlConnection sqlConnection = new SqlConnection(ConString);
+            sqlConnection.Open();
+            Console.WriteLine("Connection Opened");
+            //hard coding SQL DML query in Command Object leads to sql injection by hackers,
+            //so we should avoid it and use parameterized query instead
+            SqlCommand sqlCommand = new SqlCommand(sqlCmd, sqlConnection);
+            dataReader = sqlCommand.ExecuteReader();
+            while (dataReader.Read())
+            {
+                int StudentId = Convert.ToInt32(dataReader["StudentID"]);
+                string StudentName = dataReader["Name"].ToString();
+                string Course = dataReader["Course"].ToString();
+                int Marks = Convert.ToInt32(dataReader["Marks"]);
+                Console.WriteLine($"ID: {StudentId}, Name: {StudentName}, Course: {Course}, Marks: {Marks}");
+
+            }
+        }
+    }
+}
---
+++ ADO2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectedEnv connectedEnv = new ConnectedEnv();
+            connectedEnv.GetAllStudent();
+            Console.ReadLine();
+        }
+    }
+}
