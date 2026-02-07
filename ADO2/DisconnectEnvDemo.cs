07f963dfac491a86925cf263887bc913f724408a
gauravjha0711 <gauravjha0711@gmail.com>
2026-02-07 10:19:52 +0530

    disconnect Env using for get all student

---
+++ ADO2/DisconnectEnvDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO2
+{
+    internal class DisconnectEnvDemo
+    {
+        string ConString = "Data Source=DESKTOP-84AGQQF\\SQLEXPRESS2025;Initial Catalog = SchoolDB; Integrated Security = True;";
+        public void GetAllStudent()
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConString);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM students", sqlConnection);
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet, "students");
+            foreach (DataRow row in dataSet.Tables["students"].Rows)
+            {
+                int StudentId = Convert.ToInt32(row["StudentID"]);
+                string StudentName = row["Name"].ToString();
+                string Course = row["Course"].ToString();
+                int Marks = Convert.ToInt32(row["Marks"]);
+                Console.WriteLine($"ID: {StudentId}, Name: {StudentName}, Course: {Course}, Marks: {Marks}");
+            }
+        }
+    }
+}
--- ADO2/Program.cs
+++ ADO2/Program.cs
@@ -10,38 +10,44 @@ namespace ADO2
     {
         static void Main(string[] args)
         {
-            ConnectedEnv connectedEnv = new ConnectedEnv();
+            //ConnectedEnv connectedEnv = new ConnectedEnv();
+            //Console.WriteLine("All Student Details: ");
+            //connectedEnv.GetAllStudent();
+            //Console.WriteLine("\n\n");
+            //Console.WriteLine("Enter the id of the student you want to search for:");
+            //int id  = Convert.ToInt32(Console.ReadLine());
+            //connectedEnv.GetStudentById(id);
+
+            //Console.WriteLine("Enter details for add new students: ");
+            //Console.Write("Enter Student Id: ");
+            //int Studid = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter name: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Enter Course: ");
+            //string course = Console.ReadLine();
+            //Console.Write("Enter Marks: ");
+            //int marks = Convert.ToInt32(Console.ReadLine());
+            //connectedEnv.AddNewStudent(id, name, course, marks);
+
+            //Console.Write("Enter id for delete: ");
+            //int Delid = Convert.ToInt32(Console.ReadLine());
+            //connectedEnv.DeleteStudent(Delid);
+
+            //Console.WriteLine("Enter id for Update: ");
+            //int updId = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter name: ");
+            //string updName = Console.ReadLine();
+            //Console.Write("Enter Course: ");
+            //string updCourse = Console.ReadLine();
+            //Console.Write("Enter Marks: ");
+            //int updMarks = Convert.ToInt32(Console.ReadLine());
+            //connectedEnv.UpdateStudent(updId, updName, updCourse, updMarks);
+
+
+            DisconnectEnvDemo disconnectEnvDemo = new DisconnectEnvDemo();
             Console.WriteLine("All Student Details: ");
-            connectedEnv.GetAllStudent();
-            Console.WriteLine("\n\n");
-            Console.WriteLine("Enter the id of the student you want to search for:");
-            int id  = Convert.ToInt32(Console.ReadLine());
-            connectedEnv.GetStudentById(id);
+            disconnectEnvDemo.GetAllStudent();
 
-            Console.WriteLine("Enter details for add new students: ");
-            Console.Write("Enter Student Id: ");
-            int Studid = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter Course: ");
-            string course = Console.ReadLine();
-            Console.Write("Enter Marks: ");
-            int marks = Convert.ToInt32(Console.ReadLine());
-            connectedEnv.AddNewStudent(id, name, course, marks);
-
-            Console.Write("Enter id for delete: ");
-            int Delid = Convert.ToInt32(Console.ReadLine());
-            connectedEnv.DeleteStudent(Delid);
-
-            Console.WriteLine("Enter id for Update: ");
-            int updId = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter name: ");
-            string updName = Console.ReadLine();
-            Console.Write("Enter Course: ");
-            string updCourse = Console.ReadLine();
-            Console.Write("Enter Marks: ");
-            int updMarks = Convert.ToInt32(Console.ReadLine());
-            connectedEnv.UpdateStudent(updId, updName, updCourse, updMarks);
             Console.ReadLine();
         }
     }
