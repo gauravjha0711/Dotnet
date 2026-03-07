9efd9e8868785260e33c695e25172e603c9142f6
gauravjha0711 <gauravjha0711@gmail.com>
2026-03-07 12:17:31 +0530

    update and complete day3_6

---
+++ MVC/day3_7/EmployeeApp/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeApp.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+        public char Gender { get; set; }
+    }
+}
