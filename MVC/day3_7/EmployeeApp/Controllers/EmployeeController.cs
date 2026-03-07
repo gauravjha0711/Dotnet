6f937f91cc5901fc8fa351c26ac268e7b221b7de
gauravjha0711 <gauravjha0711@gmail.com>
2026-03-07 12:42:21 +0530

    day3_7

---
+++ MVC/day3_7/EmployeeApp/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
