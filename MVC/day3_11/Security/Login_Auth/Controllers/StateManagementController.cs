7ee915bb0c5c8084a1dd776f8c1989770e1ad970
gauravjha0711 <gauravjha0711@gmail.com>
2026-03-11 15:07:19 +0530

    Session set

--- MVC/day3_11/Security/Login_Auth/Controllers/StateManagementController.cs
+++ MVC/day3_11/Security/Login_Auth/Controllers/StateManagementController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Login_Auth.Controllers
 {
@@ -11,7 +13,7 @@ namespace Login_Auth.Controllers
         public IActionResult SetCookie()
         {
             CookieOptions option = new CookieOptions();
-            option.Expires = DateTime.Now.AddMintues(10);
+            option.Expires = DateTime.Now.AddMinutes(10);
             Response.Cookies.Append("UserName", "Gaurav Jha", option);
             return Content("Cookie has been set");
         }
@@ -24,6 +26,38 @@ namespace Login_Auth.Controllers
         {
             Response.Cookies.Delete("UserName");
             return Content("Cookie has been deleted");
+        }
+
+        //hidden field
+        public IActionResult SaveData(){
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveData(int UserId)
+        {
+            return Content($"UserId is: {UserId}");
+        }
+
+        public IActionResult Index2()
+        {
+            return RedirectToAction($"Details", new { id = 10 });
+        } 
+
+        public IActionResult Details(int id)
+        {
+            return Content($"Details of id: {id}");
+        }
+
+        public IActionResult SetSession(){
+            HttpContext.Session.SetString("UserName", "Gaurav Jha");
+            return Content("Session has been set");
+        }
+
+        public IActionResult GetSession()
+        {
+            string username = HttpContext.Session.GetString("UserName");
+            return Content($"Session Value: {username}");
         }
     }
 }
--- MVC/day3_11/Security/Login_Auth/Program.cs
+++ MVC/day3_11/Security/Login_Auth/Program.cs
@@ -1,32 +1,25 @@
-namespace Login_Auth
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
-
-            app.Run();
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
 }
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.UseSession();
+app.Run();
