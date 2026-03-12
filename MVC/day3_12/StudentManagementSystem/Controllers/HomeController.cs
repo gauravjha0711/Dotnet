6b977743acaeadbc702a9309c8d1dab58950f04d
gauravjha0711 <gauravjha0711@gmail.com>
2026-03-13 01:43:26 +0530

    home update and navbar updated

--- MVC/day3_12/StudentManagementSystem/Controllers/HomeController.cs
+++ MVC/day3_12/StudentManagementSystem/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using StudentManagementSystem.Repositories.Interfaces;
-using System.Text.Json.Serialization;
 
 namespace StudentManagementSystem.Controllers
 {
